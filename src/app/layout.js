c6adb9be5ccab5b7033468985a05b2591cd9555e
Thanh Nguyen Coder <91238806+thanhnguyencoder@users.noreply.github.com>
2024-12-02 11:39:31 +0700

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,4 @@
-import { Inter } from "next/font/google";
+//import { Inter } from "next/font/google";
 import "./globals.css";
 import 'react-toastify/dist/ReactToastify.css';
 import 'react-toastify/ReactToastify.min.css';
