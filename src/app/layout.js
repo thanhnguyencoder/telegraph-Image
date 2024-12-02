a7fc8513c8f9ea1f2b7c67b9ee2835c40c340445
Thanh Nguyen Coder <91238806+thanhnguyencoder@users.noreply.github.com>
2024-12-02 09:08:04 +0700

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -19,7 +19,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
-      <GoogleAnalytics gaId="G-JVKEXR5XSG" />
+      //<GoogleAnalytics gaId="G-JVKEXR5XSG" />
     </html>
   );
 }
