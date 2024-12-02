1e5839ec0cd74f7aba3fa534515b90a9ec406917
Thanh Nguyen Coder <91238806+thanhnguyencoder@users.noreply.github.com>
2024-12-02 11:14:46 +0700

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -501,21 +501,21 @@ export default function Home() {
               IP của bạn：<span className="text-cyan-600">{IP}</span>
             </div>
           </div>
-          <div className="flex  flex-col sm:flex-col   md:w-auto lg:flex-row xl:flex-row  2xl:flex-row  mx-auto items-center  ">
-            <span className=" text-lg sm:text-sm   md:text-sm lg:text-xl xl:text-xl  2xl:text-xl">Tải lên：</span>
-            <select
-              value={selectedOption} // 将选择框的值绑定到状态中的 selectedOption
-              onChange={handleSelectChange} // 当选择框的值发生变化时触发 handleSelectChange 函数
-              className="text-lg p-2 border  rounded text-center w-auto sm:w-auto md:w-auto lg:w-auto xl:w-auto  2xl:w-36">
-              <option value="tg" >TG(会失效)</option>
-              <option value="tgchannel">TG_Channel</option>
-              <option value="r2">R2</option>
-              {/* <option value="vviptuangou">vviptuangou</option> */}
-              <option value="58img">58img</option>
-              {/* <option value="tencent">tencent</option> */}
+          // <div className="flex  flex-col sm:flex-col   md:w-auto lg:flex-row xl:flex-row  2xl:flex-row  mx-auto items-center  ">
+          //   <span className=" text-lg sm:text-sm   md:text-sm lg:text-xl xl:text-xl  2xl:text-xl">Tải lên：</span>
+          //   <select
+          //     value={selectedOption} // 将选择框的值绑定到状态中的 selectedOption
+          //     onChange={handleSelectChange} // 当选择框的值发生变化时触发 handleSelectChange 函数
+          //     className="text-lg p-2 border  rounded text-center w-auto sm:w-auto md:w-auto lg:w-auto xl:w-auto  2xl:w-36">
+          //     <option value="tg" >TG(会失效)</option>
+          //     <option value="tgchannel">TG_Channel</option>
+          //     <option value="r2">R2</option>
+          //     {/* <option value="vviptuangou">vviptuangou</option> */}
+          //     <option value="58img">58img</option>
+          //     {/* <option value="tencent">tencent</option> */}
 
-            </select>
-          </div>
+          //   </select>
+          // </div>
 
 
         </div>
